0c1bd2a18e327c2d9d9a515b8a1dac750b79592b
Jacobo0991 <00005822@uca.edu.sv>
2023-11-14 14:21:50 -0600

    e

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express =  require('express');
 
-require('dotenv').config();
+const debug = require('debug')('students-api:server');
+
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+  require('dotenv').config();
   const envconfig = require('./src/config/env.config');
   const port = envconfig.PORT;
 
-  app.listen(port, () => {
+app.listen(port, () => {
     debug(`Server is running on port ${port}`)
   })
+
+  const database = require('./src/config/db.config');
+  database.connect();
