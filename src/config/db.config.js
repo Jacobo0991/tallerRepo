5743adf7441602c5d10713024eda9ee8f3a00e26
mariajcastsc <00005622@uca.edu.sv>
2023-11-14 14:11:20 -0600

    Conexion actualizada

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('movies-api:db');
+const debug = require('debug')('estudiantes-api:db');
 const mongoose = require('mongoose');
 const envconfig = require('./env.config')
 
